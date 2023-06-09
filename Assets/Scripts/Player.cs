0b12bbecd48307a5db1bb0fc1e730a9aa4d6bb88
Ioan Oltean <ionut.oltean@quadom.com>
2023-06-09 11:09:49 +0300

    Colision added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
     [SerializeField] private float _moveSpeed = 5;
     [SerializeField] private float _rotateSpeed = 10;
     [SerializeField] private GameInput _gameInput;
+    [SerializeField] private float _playerWidth = 1f;
+    [SerializeField] private float _playerHeight = 1f;
 
     private bool isWalking;
 
@@ -15,10 +17,42 @@ public class Player : MonoBehaviour
     {
         var inputVector = _gameInput.GetInputVector2Normalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-         isWalking = moveDir != Vector3.zero;
+        isWalking = moveDir != Vector3.zero;
         transform.forward = Vector3.Slerp(transform.forward, moveDir * _rotateSpeed, Time.deltaTime * _rotateSpeed);
-        var step = moveDir * (Time.deltaTime * _moveSpeed);
-        transform.position += step;
+        float moveDistance = Time.deltaTime * _moveSpeed;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.one * _playerHeight,
+            _playerWidth, moveDir, moveDistance);
+        if (canMove == false)
+        {
+            // cant move diagonally 
+            Vector3 xDirection = new Vector3(moveDir.x, 0, 0);
+            // can move only on x ? 
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.one * _playerHeight,
+                _playerWidth, xDirection, moveDistance);
+            if (canMove)
+            {
+                moveDir = xDirection;
+            }
+            //if can't move on x try on z 
+            else
+            {
+                Vector3 zDirection = new Vector3(0, 0, moveDir.z);
+                canMove = !Physics.CapsuleCast(transform.position,
+                    transform.position + Vector3.one * _playerHeight,
+                    _playerWidth, zDirection, moveDistance);
+                //can move on z ?
+                if (canMove)
+                {
+                    moveDir = zDirection;
+                }
+            }
+        }
+
+        var step = moveDir * moveDistance;
+        if (canMove)
+        {
+            transform.position += step;
+        }
     }
 
     public bool IsWalking()
